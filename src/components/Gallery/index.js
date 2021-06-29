f8974de783ab4e704916a6e4daa6261bd00ce4d1
Stevtm <steventmaddison@outlook.com>
2021-06-28 23:25:18 -0400

    add filter and map to only show images for selected category

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -9,7 +9,7 @@ const Gallery = ({ currentCategory }) => {
 		<section>
 			<h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
 			<p>{description}</p>
-			<PhotoList></PhotoList>
+			<PhotoList category={currentCategory.name}></PhotoList>
 		</section>
 	);
 };
