c06f9bd1350010bb30059645a13880e4191bc077
Stevtm <steventmaddison@outlook.com>
2021-07-04 21:00:39 -0400

    fix gallery bug

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -126,7 +126,7 @@ const PhotoList = ({ category }) => {
 					.map((image, i) => {
 						return (
 							<img
-								src={require(`../../assets/small/${category}/${i}.jpg`)}
+								src={require(`../../assets/small/${category}/${i}.jpg`).default}
 								alt={image.name}
 								className="img-thumbnail mx-1"
 								key={image.name}
