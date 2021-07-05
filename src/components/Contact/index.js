624eb9bf5f1a9a9903e10366550dbe5f37b95d96
Stevtm <steventmaddison@outlook.com>
2021-07-04 21:24:36 -0400

    add form divs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,9 +5,19 @@ const ContactForm = () => {
 		<section>
 			<h1>Contact me</h1>
 			<form id="contact-form">
-				{/* name input */}
-				{/* email input */}
-				{/* message text area */}
+				<div>
+					<label htmlFor="name">Name:</label>
+					<input type="text" name="name"></input>
+				</div>
+				<div>
+					<label htmlFor="email">Email address:</label>
+					<input type="email" name="email"></input>
+				</div>
+				<div>
+					<label htmlFor="message">Message:</label>
+					<input name="message" rows="5"></input>
+				</div>
+				<button type="submit">Submit</button>
 			</form>
 		</section>
 	);
