8a8f132d9eed8dc80c8c0bb0acef6ac3a4cb94f3
Stevtm <steventmaddison@outlook.com>
2021-07-05 23:02:22 -0400

    add logic to close modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -125,12 +125,14 @@ const PhotoList = ({ category }) => {
 
 	const toggleModal = (image, i) => {
 		setCurrentPhoto({ ...image, index: i });
-		setIsModalOpen(true);
+		setIsModalOpen(!isModalOpen);
 	};
 
 	return (
 		<div>
-			{isModalOpen && <Modal currentPhoto={currentPhoto}></Modal>}
+			{isModalOpen && (
+				<Modal currentPhoto={currentPhoto} onClose={toggleModal}></Modal>
+			)}
 			<div className="flex-row">
 				{photos
 					.filter((photo) => photo.category === category)
